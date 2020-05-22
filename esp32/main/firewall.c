6b533ad63a7324a6d47442b6cd3647835151c31f
Ursescu Ionut <ursescuionut97@gmail.com>
2020-05-22 16:25:52 +0300

    esp32: fixed uart port numbers, to use the default ports from mcb_firewall

--- esp32/main/firewall.c
+++ esp32/main/firewall.c
@@ -9,8 +9,8 @@
 #include "esp_log.h"
 #include "mbcontroller.h"
 
-#define MB_PORT_NUM_IN (2)     // Number of UART port used for Modbus IN connection
-#define MB_PORT_NUM_OUT (3)    // Number of UART port used for Modbus OUT connection
+#define MB_PORT_NUM_IN (UART_NUM_MAX - 1)     // Number of UART port used for Modbus IN connection
+#define MB_PORT_NUM_OUT (UART_NUM_MAX - 2)    // Number of UART port used for Modbus OUT connection
 #define MB_DEV_SPEED (115200)  // The communication speed of the UART
 
 static const char* TAG = "MODBUS_FIREWALL";
@@ -33,15 +33,15 @@ void app_main() {
                                     CONFIG_MB_FIREWALL_IN_UART_RXD, UART_PIN_NO_CHANGE, 
                                     UART_PIN_NO_CHANGE));
 
-    ESP_ERROR_CHECK(uart_set_pin(MB_PORT_NUM_IN, CONFIG_MB_FIREWALL_OUT_UART_TXD, 
+    ESP_ERROR_CHECK(uart_set_pin(MB_PORT_NUM_OUT, CONFIG_MB_FIREWALL_OUT_UART_TXD, 
                                     CONFIG_MB_FIREWALL_OUT_UART_RXD, UART_PIN_NO_CHANGE, 
                                     UART_PIN_NO_CHANGE));
 
     ESP_ERROR_CHECK(mbc_firewall_start());
 
 
-    ESP_ERROR_CHECK(mbc_firewall_destroy());
+    // ESP_ERROR_CHECK(mbc_firewall_destroy());
 
 
-    ESP_LOGI(TAG, "Firewall distroyed\n");
+    // ESP_LOGI(TAG, "Firewall distroyed\n");
 }
