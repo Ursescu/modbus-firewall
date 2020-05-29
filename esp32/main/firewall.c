ad88853fa83d9ead6ce956dd1cb9343c2286f6c4
Ursescu Ionut <ursescuionut97@gmail.com>
2020-05-29 14:45:44 +0300

    esp32: start implementing the firewall callback system

--- esp32/main/firewall.c
+++ esp32/main/firewall.c
@@ -1,12 +1,33 @@
 
 #include "firewall.h"
-#include "mbproto.h"
 
+/* Allow to see verbose output logging */
+#define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
+#include "esp_log.h"
 
 static const char *TAG = "MODBUS_FIREWALL";
 
-#define MB_FRAME_FUNCTION_CODE 0
-#define MB_FRAME_DATA 1
+#define MB_PDU_SIZE_MAX     253 /*!< Maximum size of a PDU. */
+#define MB_PDU_SIZE_MIN     1   /*!< Function Code */
+#define MB_PDU_FUNC_OFF     0   /*!< Offset of function code in PDU. */
+#define MB_PDU_DATA_OFF     1   /*!< Offset for response data in PDU. */
+
+#define MB_FUNC_NONE (0)
+#define MB_FUNC_READ_COILS (1)
+#define MB_FUNC_READ_DISCRETE_INPUTS (2)
+#define MB_FUNC_WRITE_SINGLE_COIL (5)
+#define MB_FUNC_WRITE_MULTIPLE_COILS (15)
+#define MB_FUNC_READ_HOLDING_REGISTER (3)
+#define MB_FUNC_READ_INPUT_REGISTER (4)
+#define MB_FUNC_WRITE_REGISTER (6)
+#define MB_FUNC_WRITE_MULTIPLE_REGISTERS (16)
+#define MB_FUNC_READWRITE_MULTIPLE_REGISTERS (23)
+#define MB_FUNC_DIAG_READ_EXCEPTION (7)
+#define MB_FUNC_DIAG_DIAGNOSTIC (8)
+#define MB_FUNC_DIAG_GET_COM_EVENT_CNT (11)
+#define MB_FUNC_DIAG_GET_COM_EVENT_LOG (12)
+#define MB_FUNC_OTHER_REPORT_SLAVEID (17)
+#define MB_FUNC_ERROR (128)
 
 #define MB_FIREWALL_MAX_RULES 16
 
@@ -18,19 +39,19 @@ typedef enum mb_firewall_rule_result {
     FIREWALL_PASS
 } mb_firewall_rule_result_t;
 
-typedef mb_firewall_rule_result_t ( *mb_firewall_rule_handler )(uint8_t *, uint16_t);
+typedef mb_firewall_rule_result_t (*mb_firewall_rule_handler)(uint8_t *, uint16_t);
 
 typedef struct mb_firewall_rule {
     unsigned char mb_function_code;
     mb_firewall_rule_handler handler;
 } mb_firewall_rule_t;
 
-
 /* Pass everything */
 static mb_firewall_rule_result_t pass(uint8_t *frame, uint16_t len) {
-    ESP_LOGI(TAG, "Function code handler");
+    ESP_LOGI(TAG, "handling the function code");
     return FIREWALL_PASS;
 }
+
 
 mb_firewall_rule_t firewall_rules[MB_FIREWALL_MAX_RULES] = {
     {MB_FUNC_OTHER_REPORT_SLAVEID, pass},
@@ -45,18 +66,13 @@ mb_firewall_rule_t firewall_rules[MB_FIREWALL_MAX_RULES] = {
     {MB_FUNC_READ_DISCRETE_INPUTS, pass},
 };
 
-
 /* Write the firewall function rule */
-static char firewall_cb(unsigned char addr, unsigned char *frame, unsigned short len) {
-
-    uint8_t function_code = frame[MB_FRAME_FUNCTION_CODE];
+char firewall_cb(unsigned char addr, unsigned char *frame, unsigned short len) {
+    uint8_t function_code = frame[MB_PDU_FUNC_OFF];
     mb_firewall_rule_result_t status = FIREWALL_PASS;
 
     uint8_t rule_index;
-    ESP_LOGI(TAG, "Firewall checker - packet inspection");
-    ESP_LOGI(TAG, "addr %02X, len %hu, fcode %02X", addr, len, function_code);
-
-    /* Waterfall scheme firewall */
+    ESP_LOGI(TAG, "packet received: addr 0x%02X, len %hu, fcode 0x%02X", addr, len, function_code);
 
     /* Destination address check */
     if (addr == 4) {
@@ -67,9 +83,7 @@ static char firewall_cb(unsigned char addr, unsigned char *frame, unsigned short
         /* Handle the rule found for the function code */
         if (firewall_rules[rule_index].mb_function_code == function_code) {
 
-            uint8_t *frame_data = &frame[MB_FRAME_DATA];
-
-            return firewall_rules[rule_index].handler(frame_data, len - 1);
+            return firewall_rules[rule_index].handler(frame, len);
         }
     }
 
