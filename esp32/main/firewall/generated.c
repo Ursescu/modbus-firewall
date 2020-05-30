8fe1a8392781d75e0a5857fb1eeaec94bb5bd4f4
Ursescu Ionut <ursescuionut97@gmail.com>
2020-05-30 18:55:30 +0300

    esp32: added sample firewall rules

--- esp32/main/firewall/generated.c
+++ esp32/main/firewall/generated.c
@@ -5,10 +5,56 @@
 extern mb_firewall_policy_t firewall_default_policy;
 extern mb_firewall_adress_t firewall_addresses[MB_FIREWALL_MAX_ADDRS];
 
-mb_firewall_policy_t firewall_default_policy = FIREWALL_BLACKLIST;
+mb_firewall_policy_t firewall_default_policy = MB_FIREWALL_WHITELIST;
 
 mb_firewall_adress_t firewall_addresses[MB_FIREWALL_MAX_ADDRS] = {
-    10, 15
+    10, 15, 1
 };
 
+/* Rules for each modbus request type */
 
+/* Coils */
+mb_firewall_rule_t firewall_coil_rules[MB_FIREWALL_MAX_RULES] = {
+    {
+        .type = MB_FIREWALL_RULE_FIXED,
+        .policy = MB_FIREWALL_WHITELIST,
+        .mode = MB_FIREWALL_REG_READ,
+        .rule_fixed = {
+            .reg_addr = 0x0002,
+            .reg_data = 0x0000,
+        },
+    },
+    {
+        .type = MB_FIREWALL_RULE_FIXED,
+        .policy = MB_FIREWALL_BLACKLIST,
+        .mode = MB_FIREWALL_REG_READ,
+        .rule_fixed = {
+            .reg_addr = 0x0001,
+            .reg_data = 0x0000,
+        },
+    },
+    {
+        .type = MB_FIREWALL_RULE_FIXED,
+        .policy = MB_FIREWALL_WHITELIST,
+        .mode = MB_FIREWALL_REG_WRITE,
+        .rule_fixed = {
+            .reg_addr = 0x0005,
+            .reg_data = 0x0001,
+        },
+    },
+};
+
+/* Discrete */
+mb_firewall_rule_t firewall_discrete_rules[MB_FIREWALL_MAX_RULES] = {
+
+};
+
+/* Inputs */
+mb_firewall_rule_t firewall_input_rules[MB_FIREWALL_MAX_RULES] = {
+
+};
+
+/* Holdings */
+mb_firewall_rule_t firewall_holding_rules[MB_FIREWALL_MAX_RULES] = {
+
+};
