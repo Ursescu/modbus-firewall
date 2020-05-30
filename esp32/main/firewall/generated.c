490b8e97efcf90a045532040d41992641e4890da
Ursescu Ionut <ursescuionut97@gmail.com>
2020-05-30 20:02:33 +0300

    esp32: added multiple rules for firewall

--- esp32/main/firewall/generated.c
+++ esp32/main/firewall/generated.c
@@ -5,10 +5,11 @@
 extern mb_firewall_policy_t firewall_default_policy;
 extern mb_firewall_adress_t firewall_addresses[MB_FIREWALL_MAX_ADDRS];
 
-mb_firewall_policy_t firewall_default_policy = MB_FIREWALL_WHITELIST;
+mb_firewall_policy_t firewall_default_policy = MB_FIREWALL_BLACKLIST;
 
 mb_firewall_adress_t firewall_addresses[MB_FIREWALL_MAX_ADDRS] = {
-    10, 15, 1};
+    10, 15,
+};
 
 /* Rules for each modbus request type */
 
@@ -50,10 +51,39 @@ mb_firewall_rule_t firewall_discrete_rules[MB_FIREWALL_MAX_RULES] = {
 
 /* Inputs */
 mb_firewall_rule_t firewall_input_rules[MB_FIREWALL_MAX_RULES] = {
-
+    {
+        .type = MB_FIREWALL_RULE_FIXED,
+        .policy = MB_FIREWALL_WHITELIST,
+        .mode = MB_FIREWALL_REG_READ,
+        .rule_fixed = {
+            .reg_addr = 0x0002,
+            .reg_data = 0x0001,
+        },
+    },
 };
 
 /* Holdings */
 mb_firewall_rule_t firewall_holding_rules[MB_FIREWALL_MAX_RULES] = {
-
+    {
+        .type = MB_FIREWALL_RULE_REG_DATA_INTERVAL,
+        .policy = MB_FIREWALL_WHITELIST,
+        .mode = MB_FIREWALL_REG_WRITE,
+        .rule_reg_data_interval = {
+            .reg_addr_start = 0x0001,
+            .reg_addr_stop = 0x0002,
+            .reg_data_start = 0xAAAA,
+            .reg_data_stop = 0XAAFA
+        },
+    },
+    {
+        .type = MB_FIREWALL_RULE_REG_DATA_INTERVAL,
+        .policy = MB_FIREWALL_WHITELIST,
+        .mode = MB_FIREWALL_REG_WRITE,
+        .rule_reg_data_interval = {
+            .reg_addr_start = 0x0001,
+            .reg_addr_stop = 0x0002,
+            .reg_data_start = 0x0000,
+            .reg_data_stop = 0XFFFA
+        },
+    },
 };
