759e46b110616b5c12b31d12c6f0a21f43d95c35
Ursescu Ionut <ursescuionut97@gmail.com>
2020-07-01 08:14:39 +0300

    esp32: modified the rules to show up the demo

--- esp32/main/firewall/generated.c
+++ esp32/main/firewall/generated.c
@@ -5,6 +5,14 @@
 extern mb_firewall_policy_t firewall_default_policy;
 extern mb_firewall_adress_t firewall_addresses[MB_FIREWALL_MAX_ADDRS];
 
+extern mb_firewall_rule_t firewall_coil_rules[MB_FIREWALL_MAX_RULES];
+
+extern mb_firewall_rule_t firewall_discrete_rules[MB_FIREWALL_MAX_RULES];
+
+extern mb_firewall_rule_t firewall_input_rules[MB_FIREWALL_MAX_RULES];
+
+extern mb_firewall_rule_t firewall_holding_rules[MB_FIREWALL_MAX_RULES];
+
 mb_firewall_policy_t firewall_default_policy = MB_FIREWALL_BLACKLIST;
 
 mb_firewall_adress_t firewall_addresses[MB_FIREWALL_MAX_ADDRS] = {
@@ -70,20 +78,31 @@ mb_firewall_rule_t firewall_holding_rules[MB_FIREWALL_MAX_RULES] = {
         .mode = MB_FIREWALL_REG_WRITE,
         .rule_reg_data_interval = {
             .reg_addr_start = 0x0001,
-            .reg_addr_stop = 0x0002,
+            .reg_addr_stop = 0x0010,
             .reg_data_start = 0xAAAA,
-            .reg_data_stop = 0XAAFA
+            .reg_data_stop = 0xAAFA
+        },
+    },
+    {
+        .type = MB_FIREWALL_RULE_REG_DATA_INTERVAL,
+        .policy = MB_FIREWALL_BLACKLIST,
+        .mode = MB_FIREWALL_REG_WRITE,
+        .rule_reg_data_interval = {
+            .reg_addr_start = 0x0001,
+            .reg_addr_stop = 0x0010,
+            .reg_data_start = 0xBBBB,
+            .reg_data_stop = 0xFFFF
         },
     },
     {
         .type = MB_FIREWALL_RULE_REG_DATA_INTERVAL,
         .policy = MB_FIREWALL_WHITELIST,
-        .mode = MB_FIREWALL_REG_WRITE,
+        .mode = MB_FIREWALL_REG_READ,
         .rule_reg_data_interval = {
             .reg_addr_start = 0x0001,
-            .reg_addr_stop = 0x0002,
+            .reg_addr_stop = 0x0010,
             .reg_data_start = 0x0000,
-            .reg_data_stop = 0XFFFA
+            .reg_data_stop = 0x0000
         },
     },
 };
